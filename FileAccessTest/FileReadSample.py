303fb3840a5494614517ee604a4379ab2e8b0bf5
Hoshino Hitoshi <hhoshino-main@memoad.jp>
2018-05-09 11:04:46 +0900

    refactoring commit.(Encoding,Line sep modify)

--- FileAccessTest/FileReadSample.py
+++ FileAccessTest/FileReadSample.py
@@ -1,44 +1,47 @@
 
+
+# １行ずつ読み込み１行ずつ出力するパターン
 def pattern_1():
     # ファイルをオープンする
-    test_data = open("test_data.txt", "r")
+    test_data = open("test_data.txt", "r", encoding="utf_8")
 
     # 一行ずつ読み込んで表示する
     for line in test_data:
-        print(line)
+        print(line, end="")
 
     # ファイルをクローズする
     test_data.close()
 
 
+# 全て読み込み１行ごとにリストデータにしてから出力するパターン
 def pattern_2():
     # ファイルをオープンする
-    test_data = open("test_data.txt", "r")
+    test_data = open("test_data.txt", "r", encoding="utf_8")
 
     # 行ごとにすべて読み込んでリストデータにする
     lines = test_data.readlines()
 
     # 一行ずつ表示する
     for line in lines:
-        print(line)
+        print(line, end="")
 
     # ファイルをクローズする
     test_data.close()
 
+
+# 全て読み込み全てのデータを一度に出力するパターン
 def pattern_3():
     # ファイルをオープンする
-    test_data = open("test_data.txt", "r")
+    test_data = open("test_data.txt", "r", encoding="utf_8")
 
     # すべての内容を読み込む
     contents = test_data.read()
 
     # すべての内容を表示する
-    print(contents)
-
+    print(contents, end="")
 
     # ファイルをクローズする
     test_data.close()
-
 
 
 # プログラムのエントリポイント
